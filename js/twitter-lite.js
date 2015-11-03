1eb836f0c98114fd6c6b10cdbdf5ad9b92772a82
Josh Lehman <jalehman37@gmail.com>
2015-11-02 20:33:48 -0800

    Initial working commit.

---
+++ js/twitter-lite.js
@@ -0,0 +1,66 @@
+// ==========================================
+// Global Variables (use sparingly)
+// ==========================================
+
+/*
+ * Rathering than actually performing a network request, we'll just store some
+ * sample data in a tweets.json file. This simplifies development so we can
+ * easily play around with different data and not rely on complicated
+ * authentication mechanisms with actual servers.
+ */
+var tweetsResource = "data/tweets.json";
+
+// ==========================================
+// Helper Functions
+// ==========================================
+
+/*
+ * Given two single-argument functions f and g, return f(g(x))
+ */
+function compose(f, g) {
+    return function(x) {
+        return f(g(x));
+    };
+}
+
+// ==========================================
+// HTML Generation
+// ==========================================
+
+function li(content) {
+    return "<li>" + content + "</li>";
+}
+
+function p(content) {
+    return "<p>" + content +"</p>";
+}
+
+// ==========================================
+// Fetch/Parse "Tweets"
+// ==========================================
+
+function tweetContent(tweet) {
+    return tweet.body;
+}
+
+function loadTweets(cb) {
+    $.ajax({
+        url: tweetsResource,
+        method: 'GET',
+        success: cb
+    });
+}
+
+// ==========================================
+// DOM Manipulation
+// ==========================================
+
+$("input#load-tweets-btn").click(function(e) {
+    var button = $( e.target );
+    button.prop("value", "Loading...");
+    loadTweets(function(tweets) {
+        button.prop("value", "Refresh Tweets");
+        var render = compose(p, tweetContent);
+        $("ul#tweets").html(_.map(tweets, render));
+    });
+});
